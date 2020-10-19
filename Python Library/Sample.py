c196258a947130cc38711f13aef56e28dcbd4278
RAJA AHMED <ahmedrajay@gmail.com>
2020-10-19 13:14:12 +0500

    Update Sample.py

--- Python Library/Sample.py
+++ Python Library/Sample.py
@@ -1,9 +1,4 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+a = 10
+b = 4
+c = a + b
+print(c)
