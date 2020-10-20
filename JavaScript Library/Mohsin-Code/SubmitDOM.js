d3c6d77ed4fef3ca5fa8d4d311c1e5d3e55b54d1
Mohsin <59390499+mohsin-code@users.noreply.github.com>
2020-10-20 17:05:43 +0500

    Remove Script

--- JavaScript Library/Mohsin-Code/SubmitDOM.js
+++ JavaScript Library/Mohsin-Code/SubmitDOM.js
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head>
-<script>
-function confirmInput() {
-  fname = document.forms[0].fname.value;
-  document.write("Hello " + fname + "! DO you like my application?");
-}
-</script>
-</head>
 <body>
 
-<form onsubmit="confirmInput()" action="https://mohsin-code.github.io/">
+<form onsubmit="confirmInput()">
   Enter your name: <input id="fname" type="text" size="20">
   <input type="submit">
 </form>
