d8472801b36856361f5675518e2d05e1da8099cc
Hasnain Ahmed Khan <73159324+HasnainAhmedKhan@users.noreply.github.com>
2020-10-20 12:39:24 +0500

    Create Output of Sum
    C++ File

---
+++ C++ Library/Output Function/Output2Var.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //Preprocessor Directive
+#include<math.h>
+
+using namespace std;
+
+int main()
+{   //start the program
+    int a=2, b=3;   //assigning variables
+    float sum = a + b + pi; //operating on variables
+    cout<<a<<" + "<<b<<" = "<<sum; //display result
+}   //end the program
