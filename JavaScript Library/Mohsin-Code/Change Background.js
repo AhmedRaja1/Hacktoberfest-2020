a0aef0d1ee15cd312b01bd04b4ec20409f5baca7
Mohsin <59390499+mohsin-code@users.noreply.github.com>
2020-10-20 17:27:13 +0500

    Delete Change Background.js

--- JavaScript Library/Mohsin-Code/Change Background.js
+++
@@ -1,5 +0,0 @@
-function changeBg() {
-        let colors = [`red`,`green`,`blue`,`pink`,`orange`,`purple`,`black`,`white`,`brown`,`yellow`];
-        let index = parseInt(Math.random() * colors.length);   
-        document.body.style.backgroundColor = colors[index]; 
-    }
