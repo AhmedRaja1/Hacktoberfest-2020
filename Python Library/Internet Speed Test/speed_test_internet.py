1d84601b382ac2ba24e9ab8d2860246aa8def53e
Abdul Rehman <33784045+arehman98@users.noreply.github.com>
2020-10-22 01:01:45 +0500

    Upload function call correction

--- Python Library/Internet Speed Test/speed_test_internet.py
+++ Python Library/Internet Speed Test/speed_test_internet.py
@@ -2,9 +2,11 @@ import speedtest
 
 st = speedtest.Speedtest()
 
+print("After analyzing your network")
+
 print("download speed",st.download())
 
-print("upload speed is", st.download())
+print("upload speed is", st.upload())
 
 st.get_servers([])
 print(st.results.ping)
