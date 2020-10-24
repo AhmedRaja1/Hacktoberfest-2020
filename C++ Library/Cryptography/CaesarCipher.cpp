ff9e0760d971e2d769b7ad09bfb5017d50bcb88f
Hamza Mateen <hamzamateen1583@gmail.com>
2020-10-24 09:40:08 +0500

    Added CaesarCipher to Cryptography with encryption method

---
+++ C++ Library/Cryptography/CaesarCipher.cpp
@@ -0,0 +1,55 @@
+/**
+ * CaesarCipher - A shift-substitution cipher
+ * author@HamzaMateen
+**/
+
+#include <iostream>
+#include <string>
+
+using namespace std;   
+string encrypt(string text, int shiftCount) 
+{
+    // getting the key ready 
+    string alphabets = "abcdefghijklmnopqrstuvwxyz";
+    
+    string end = alphabets.substr(26 - shiftCount, 26);
+    
+    string keyText = end + alphabets.substr(0,26-shiftCount);
+    
+    // encrypting  
+    string encText = "";
+    
+    for (unsigned int i = 0; i < text.length(); i++) 
+    {
+        if (int(text[i]) == 32) continue;
+        else 
+encText += keyText[alphabets.find(text[i])];
+     }
+    
+    // return the encrypted text 
+    return encText;
+}
+
+// driver program
+int main()
+{
+    int key;
+    std::string text;
+    
+    std::cout << "[input]Enter the text to be encrypted" << std::endl;
+    
+    std::cin >> text;
+    
+    std::cout << "[input] Enter the key or shift\n\n" << std::endl;
+    
+    std::cin >> key;
+    while (key < 1 || key > 27) {
+        std::cout << "Please entery the key b/w 0-26";
+        std::cin >> key;
+    }
+    std::cout << "[INPUT] " << text << std::endl;
+    std::cout << "[ENCRYPTED] " << encrypt(text, key) << std::endl;
+    
+    
+    return 0;
+}
