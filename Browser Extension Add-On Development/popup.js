cff9d23b9f5941f9eccea04407935e7f0fc71bb0
RAJA AHMED <ahmedrajay@gmail.com>
2021-10-10 18:12:48 +0500

    Delete Browser Extension Add-On Development directory

--- Browser Extension Add-On Development/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btn').addEventListener('click', () => {
-	chrome.runtime.sendMessage({interval:document.getElementById('text').value}, function(response) {
-		console.log(response, 'popup');
-	});
-})
